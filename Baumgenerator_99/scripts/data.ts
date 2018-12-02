6b27396dd8d9af56bb9d84733dbadf57a2b64277
monettamarchiano <monettamarchiano@monettas-mbp.fritz.box>
2018-12-02 16:56:41 +0100

    new

--- Baumgenerator_99/scripts/data.ts
+++ Baumgenerator_99/scripts/data.ts
@@ -25,46 +25,46 @@ namespace wbk_99 {
 
         {
             "Baumart": [
-                { name: "Tanne", price: 100.0 },
-                { name: "Fichte", price: 0.0 },
-                { name: "Kiefer", price: 0.0 },
-                { name: "Plastik", price: 0.0 }
+                { name: "Tanne", price: 30.0 },
+                { name: "Fichte", price: 30.0 },
+                { name: "Kiefer", price: 30.0 },
+                { name: "Plastik", price: 20.0 }
 
             ],
 
             "Halterung": [
                 { name: "keine", price: 0.0 },
-                { name: "Seilzug", price: 0.0 },
-                { name: "Klemmschraube", price: 0.0 },
-                { name: "Klemmbacken", price: 0.0 }
+                { name: "Seilzug", price: 30.0 },
+                { name: "Klemmschraube", price: 30.0 },
+                { name: "Klemmbacken", price: 30.0 }
             ],
 
             "Beleuchtung": [
-                { name: "Kerze weiss", price: 0.0 },
-                { name: "Kerze rot", price: 0.0 },
-                { name: "Kerze Bienenwachs", price: 0.0 },
-                { name: "20er Lichterkette kaltweiß", price: 0.0 },
-                { name: "20er Lichterkette warmweis", price: 0.0 }
+                { name: "Kerze weiss", price: 1.0 },
+                { name: "Kerze rot", price: 1.0 },
+                { name: "Kerze Bienenwachs", price: 1.0 },
+                { name: "20er Lichterkette kaltweiß", price: 10.0 },
+                { name: "20er Lichterkette warmweis", price: 10.0 }
             ],
 
             "Glaskugeln": [
-                { name: "silber", price: 0.0 },
-                { name: "gold", price: 0.0 },
-                { name: "rot", price: 0.0 },
-                { name: "weiß", price: 0.0 },
-                { name: "grün", price: 0.0 }
+                { name: "silber", price: 3.0 },
+                { name: "gold", price: 3.0 },
+                { name: "rot", price: 3.0 },
+                { name: "weiß", price: 3.0 },
+                { name: "grün", price: 3.0 }
             ],
             
             "Schmuck": [
-                { name: "Engel", price: 0.0 },
-                { name: "Stern", price: 0.0 },
-                { name: "Herz", price: 0.0 }
+                { name: "Engel", price: 3.0 },
+                { name: "Stern", price: 3.0 },
+                { name: "Herz", price: 3.0 }
             ],
 
             "Lieferdienst": [
-                { name: "DHL", price: 4.99 },
-                { name: "UPS", price: 5.95 },
-                { name: "Hermes", price: 3.0 }
+                { name: "DHL", price: 5.00 },
+                { name: "UPS", price: 5.00 },
+                { name: "Hermes", price: 5.0 }
             ],
             
             "Lieferart": [
