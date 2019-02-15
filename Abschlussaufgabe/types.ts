80a2ed836eeedbaff3669c78127d0c316c048a3b
monettamarchiano <monettamarchiano@monettas-mbp.eduroam.hs-furtwangen.de>
2019-02-15 17:55:03 +0100

    neue schrift

--- Abschlussaufgabe/types.ts
+++ Abschlussaufgabe/types.ts
@@ -1,3 +1,10 @@
+/* Aufgabe: Abschlussaufgabe
+Name: Monetta Marchiano
+Matrikel: 256063
+Datum: 11.02.2019
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert. */
+
 interface ScoreData {
     [key: string]: string;
 }
